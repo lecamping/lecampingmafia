85685be01edaebedeadee991bd05db90a0091be9
Steren <steren.giannini@gmail.com>
2014-04-15 11:19:28 -0400

    fixed href links (generate mailto: URLs)

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ mafiapp.factory('Startup', function($resource) {
 mafiapp.controller('StartupCtrl', function($scope, Startup) {
   $scope.startups = Startup.query();
 });
+
+// transforms email addresses to mailto: strings
+mafiapp.filter('mailto', function() {
+  var replacePattern = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+  
+    return function(text) {        
+      if(text.match(replacePattern)) {
+        text = text.replace(replacePattern, "mailto:$1");
+      }
+      return text;        
+    };
+	});
